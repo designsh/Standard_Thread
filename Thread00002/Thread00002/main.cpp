d0f6fc42833a6e8f13fc01b19d1ed42721df07f8
designsh94 <designsh94@naver.com>
2022-12-06 13:17:11 +0900

    신종하 : 한개의 솔루션에 여러개의 프로젝트 생성 커밋

---
+++ Thread00002/Thread00002/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+/// <summary>
+/// std::thread 기본사용법
+/// </summary>
+
+void Function()
+{
+	int a = 0;
+	cout << "Function " << endl;
+}
+
+int main()
+{
+	cout << "Thread00002 Process Start" << endl;
+
+	thread t1(Function);
+	thread t2(Function);
+	thread t3([]
+		{
+			int a = 0;
+			cout << "lambda Function" << endl;
+		});
+
+	t1.join();
+	t2.join();
+	t3.join();
+
+	cout << "Thread00002 Process End" << endl;
+	system("pause");
+	return 0;
+}
